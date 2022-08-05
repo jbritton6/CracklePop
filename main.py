9ced886985b2353645a0de9b8ffc1e55a5b71048
Jacob Britton <jacobbritton95@gmail.com>
2022-08-05 07:10:17 -0400

    Create docstring

---
+++ main.py
@@ -0,0 +1,15 @@
+def crackle_pop(number):
+    """FizzBuzz like program for numbers from 1 to numbers.
+    
+    Args:
+        number: Integer value greater than 1.
+
+    Returns:
+        List of numbers from 1 to numbers according to the followung:
+          Crackle if it is divisble by 3,
+          Pop if it is divisble by 5,
+          CracklePop if it is divisble by 3 and 5.
+
+    Raises:
+        ValueError: If input is less than 1.
+    """
